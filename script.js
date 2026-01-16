38bb01dd59ba887a9b607d9aed3ada02ef23a6ce
Pooja Jadhav <pgjadhav603@gmail.com>
2026-01-16 14:12:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+ const squares = document.querySelectorAll(".square");
+
+    squares.forEach((current) => {
+      current.addEventListener("mouseenter", () => {
+        squares.forEach((sq) => {
+          if (sq !== current) {
+            sq.style.backgroundColor = "#6F4E37"; // Coffee
+          }
+        });
+      });
+
+      current.addEventListener("mouseleave", () => {
+        squares.forEach((sq) => {
+          sq.style.backgroundColor = "#E6E6FA"; // Lavender
+        });
+      });
+    });
